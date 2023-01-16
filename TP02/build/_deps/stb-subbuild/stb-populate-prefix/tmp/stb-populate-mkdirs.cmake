af3995f83b05ef264ba5c7f4cd9f125e35bf4af4
Yahya MAMOUNI <yamamouni@local.isima.fr>
2023-01-16 09:42:14 +0100

    mandlebrot added

--- TP02/build/_deps/stb-subbuild/stb-populate-prefix/tmp/stb-populate-mkdirs.cmake
+++ TP02/build/_deps/stb-subbuild/stb-populate-prefix/tmp/stb-populate-mkdirs.cmake
@@ -4,19 +4,19 @@
 cmake_minimum_required(VERSION 3.5)
 
 file(MAKE_DIRECTORY
-  "C:/Users/Yahya/Desktop/GPGPU-master/GPGPU/TP02/build/_deps/stb-src"
-  "C:/Users/Yahya/Desktop/GPGPU-master/GPGPU/TP02/build/_deps/stb-build"
-  "C:/Users/Yahya/Desktop/GPGPU-master/GPGPU/TP02/build/_deps/stb-subbuild/stb-populate-prefix"
-  "C:/Users/Yahya/Desktop/GPGPU-master/GPGPU/TP02/build/_deps/stb-subbuild/stb-populate-prefix/tmp"
-  "C:/Users/Yahya/Desktop/GPGPU-master/GPGPU/TP02/build/_deps/stb-subbuild/stb-populate-prefix/src/stb-populate-stamp"
-  "C:/Users/Yahya/Desktop/GPGPU-master/GPGPU/TP02/build/_deps/stb-subbuild/stb-populate-prefix/src"
-  "C:/Users/Yahya/Desktop/GPGPU-master/GPGPU/TP02/build/_deps/stb-subbuild/stb-populate-prefix/src/stb-populate-stamp"
+  "C:/Users/yamamouni/Desktop/GPGPU/TP02/build/_deps/stb-src"
+  "C:/Users/yamamouni/Desktop/GPGPU/TP02/build/_deps/stb-build"
+  "C:/Users/yamamouni/Desktop/GPGPU/TP02/build/_deps/stb-subbuild/stb-populate-prefix"
+  "C:/Users/yamamouni/Desktop/GPGPU/TP02/build/_deps/stb-subbuild/stb-populate-prefix/tmp"
+  "C:/Users/yamamouni/Desktop/GPGPU/TP02/build/_deps/stb-subbuild/stb-populate-prefix/src/stb-populate-stamp"
+  "C:/Users/yamamouni/Desktop/GPGPU/TP02/build/_deps/stb-subbuild/stb-populate-prefix/src"
+  "C:/Users/yamamouni/Desktop/GPGPU/TP02/build/_deps/stb-subbuild/stb-populate-prefix/src/stb-populate-stamp"
 )
 
 set(configSubDirs Debug)
 foreach(subDir IN LISTS configSubDirs)
-    file(MAKE_DIRECTORY "C:/Users/Yahya/Desktop/GPGPU-master/GPGPU/TP02/build/_deps/stb-subbuild/stb-populate-prefix/src/stb-populate-stamp/${subDir}")
+    file(MAKE_DIRECTORY "C:/Users/yamamouni/Desktop/GPGPU/TP02/build/_deps/stb-subbuild/stb-populate-prefix/src/stb-populate-stamp/${subDir}")
 endforeach()
 if(cfgdir)
-  file(MAKE_DIRECTORY "C:/Users/Yahya/Desktop/GPGPU-master/GPGPU/TP02/build/_deps/stb-subbuild/stb-populate-prefix/src/stb-populate-stamp${cfgdir}") # cfgdir has leading slash
+  file(MAKE_DIRECTORY "C:/Users/yamamouni/Desktop/GPGPU/TP02/build/_deps/stb-subbuild/stb-populate-prefix/src/stb-populate-stamp${cfgdir}") # cfgdir has leading slash
 endif()
--- TP02/src/gpgpu/gpgpu.cu
+++ TP02/src/gpgpu/gpgpu.cu
@@ -1,7 +1,7 @@
 #include <gpgpu.h>
 #include <algorithm>
 #include <iostream>
-
+#include <cuComplex.h>
 
 
 //Kernel gray-scale
@@ -10,35 +10,21 @@ __global__ void kernel_gray(float * device_image_float){
     int32_t index = blockIdx.x * blockDim.x + threadIdx.x;
     device_image_float[index] = device_image_float[index] + 0.5f;
 }
-/*
-// Kernel uv
-__global__ void kernel_uv(float * image, int32_t width, int32_t height){
-    //int32_t x = blockIdx.x * blockDim.x + threadIdx.x;
-    //int32_t y = blockIdx.y * blockDim.y + threadIdx.y;
-    int index;
-    for (int j = 0; j < height; j++){
-        for (int i = 0 ; i < width ; ++i){
-                index = 3 * (width * j + i);
-                float u = (float)i / (float)width;
-                float v = (float)j / (float)height;
-                // (1-u) and (1-v) is added to get the right values because if we keep u and v we will get the image but inverted (Ask teacher)
-                // Is it a thing in the gpu ? because u & v worked fine in TP1
-                int ir = int(255.0 * (1-u));
-                int ig = int(255.0 * (1-v));
 
-                image[index++] = ir;
-                image[index++] = ig;
-                image[index++] = 0;
-        }
-    }
-
-}*/
 
 __global__ void kernel_uv(float * image, int32_t width, int32_t height){
     int32_t x = blockIdx.x * blockDim.x + threadIdx.x;
     int32_t y = blockIdx.y * blockDim.y + threadIdx.y;
+
+
     int index;
     index = 3 * (width * y + x);
+    // Local position
+    //x = threadIdx.x;
+    //y = threadIdx.y;
+    // Local color
+    //float u = (float)x / (float)32;
+    //float v = (float)y / (float)32;
     float u = (float)x / (float)width;
     float v = (float)y / (float)height;
     int ir = int(255.0 * (1-u));
@@ -52,6 +38,36 @@ __global__ void kernel_uv(float * image, int32_t width, int32_t height){
 
 }
 
+//kernel_mandelbrot
+__global__ void kernel_mandelbrot(float* image) {
+    // we need this for the index
+    int32_t x = blockIdx.x * blockDim.x + threadIdx.x;
+    int32_t y = blockIdx.y * blockDim.y + threadIdx.y;
+    int index;
+    index = 3 * (1024 * y + x);
+    // this is the counter that we use in while
+    unsigned int cmp = 0u;
+    // We limit our x & y to a number between 0 and 1 and we give it to u and v
+    // Because the mandelbrot accept values between -2 and 2
+    float u = float(x) / 1024;
+    float v = float(y) / 1024;
+    // c is the complex related to u,v
+    // z is an init value of (0.f, 0.f) in the complex
+    cuFloatComplex c = make_cuFloatComplex(u, v);
+    cuFloatComplex z = make_cuFloatComplex(0.f,0.f);
+    // Then we loop 
+    while (cuCabsf(z) < 1.f && cmp <= 400) {
+        z = cuCaddf(cuCmulf(z, z), c);
+        cmp++;
+    }
+
+    image[index++] = cuCabsf(z);
+    image[index++] = 0;
+    image[index++] = 0;
+
+
+}
+
 
 void GetGPGPUInfo(){
     cudaDeviceProp cuda_properties;
@@ -62,6 +78,7 @@ void GetGPGPUInfo(){
 
 void GenerateGrayscaleImage(std::vector<uint8_t>& host_image_uint8, int32_t width, int32_t height){
     std::vector<float> host_image_float;
+    host_image_float.reserve(host_image_uint8.size());
     // Filling host_image_float with the content of host_image_uint8
     // Using std::transform
     // the y argument will be the corresponding value in the output image.
@@ -90,7 +107,10 @@ void GenerateGrayscaleImage(std::vector<uint8_t>& host_image_uint8, int32_t widt
     // Call the kernel uv
     dim3 threads(32,32);
     dim3 blocks(32, 32);
-    kernel_uv <<<blocks, threads>>>(device_image_float, width, height);
+    //kernel_uv <<<blocks, threads>>>(device_image_float, width, height);
+
+    // Mandelbrot
+    kernel_mandelbrot <<<blocks, threads>>> (device_image_float);
 
     cudaMemcpy(
         host_image_float.data(),
